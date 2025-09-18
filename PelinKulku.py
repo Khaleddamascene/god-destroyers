3562c11e4c1f96ea09351ca8cbd0343750af56f4
Trollipopper <113014080+Trollipopper@users.noreply.github.com>
2025-09-18 15:04:26 +0300

    Perus pelinkulku juttu

---
+++ PelinKulku.py
@@ -0,0 +1,26 @@
+
+game_name = "Minecraft"
+
+print(game_name)
+print("Start / Quit")
+inp = ""
+while True:
+    if inp == "Start":
+        break
+    elif inp == "Quit":
+        #Quit the game
+        break
+    
+
+print("What is your name?")
+name = input()
+
+print("What continent will you start in?")
+continent = input()
+
+print("What country in " + continent + "?")
+country = input()
+
+print("What difficulty? (Easy/Medium/Hard)")
+difficulty = input()
+
